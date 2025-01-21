ec6d65c606e89d1394cf2b5e5e00b7a4dbd9df2d
santipong1403 <sunti_bbb.13220@hotmail.com>
2025-01-21 09:55:56 +0700

    Initial commit

--- index.ts
+++ index.ts
@@ -153,4 +153,3 @@ app.get('/rainfall_daily', async (req) => {
 // เริ่มเซิร์ฟเวอร์
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API is running at http://localhost:${PORT}`));
-// ควย
